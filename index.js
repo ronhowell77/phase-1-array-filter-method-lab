236712eb25db57e2fc6d51d22741f0c74ac8bd98
Ronnie <ron.howell0326@gmail.com>
2022-10-09 07:07:53 -0400

    commit tree 0dfa3b5a7bcc56b357aa843c9aa51041e39f2aa0

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(driver, string){
+    for(const driver of drivers){
+        return driver === "Bobby"
+    }
+}
